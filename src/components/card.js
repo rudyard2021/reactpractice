17e8e151b39a9e525e3cd69ba68a38078027d21c
Rudyard2021 <rudyard2021@gmail.com>
2020-01-21 16:24:04 -0500

    Crear componentes con iteracion

--- src/components/card.js
+++ src/components/card.js
@@ -4,17 +4,16 @@ import circleImage from '../images/circles.png';
 import './styles/card.css';
 
 class Card extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            image:exerciseImage,
-        };
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         image:exerciseImage,
+    //     };
+    // }
 
     render(){
 
         const {title, description, img, leftColor, rightColor} = this.props;
-        console.log(this.props);
 
         return (
             <div className="card mx-auto fitness-card"
@@ -25,7 +24,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row-center">
                         <div className="col-6">
-                            <img src= {this.state.image} className="fitness-imgsize float-right"/>
+                            <img src= {img} className="fitness-imgsize float-right"/>
                         </div>
                         <div className="col-6 fitness-card-info">
                             <h1>{title}</h1>
